08210521b66c7005b0b92002cdfe4497f59882be
shreya miriam <shreyamiriamroy@gmail.com>
2024-11-28 13:27:13 +0530

    first commit

---
+++ main/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    task=models.CharField(max_length=255)
+    is_completed=models.BooleanField(default=False)
+    created_date=models.DateField(auto_now_add=True)
+    updated_date=models.DateField(auto_now=True)
